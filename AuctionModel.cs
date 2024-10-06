a854a7a9e1e31baff41c20480f247b410dad94c6
priyanshibhimani <priyanshi@weybee.in>
2024-10-06 21:48:15 +0530

    first commit

--- AuctionModel.cs
+++ AuctionModel.cs
@@ -1,30 +1,3 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//namespace AuctionDemo
-//{
-//    public class AuctionModel
-//    {
-//        public string Id { get; set; }
-//        public string Title { get; set; }
-//        public string Description { get; set; }
-//        public string ImageUrl { get; set; }
-//        public  string Link { get; set; }
-//        public string LotCount { get; set; }
-//        public string StartDate { get; set; }
-//        public string StartMonth { get; set; }
-//        public string StartYear { get; set; }
-//        public string StartTime { get; set; }
-//        public string EndDate { get; set; }
-//        public string EndMonth { get; set; }
-//        public string EndYear { get; set; }
-//        public string EndTime { get; set; }
-//        public string Location { get; set; }
-//    }
-//}
 using System;
 using System.Collections.Generic;
 using System.Linq;
